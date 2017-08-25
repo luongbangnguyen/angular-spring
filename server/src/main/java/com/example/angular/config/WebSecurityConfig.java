7e901e1d1330f71c90959cebae2056c996034a96
bang nguyen luong <luongbangvh@gmail.com>
2017-08-25 18:26:52 +0700

    update out dir angular client

--- server/src/main/java/com/example/angular/config/WebSecurityConfig.java
+++ server/src/main/java/com/example/angular/config/WebSecurityConfig.java
@@ -13,4 +13,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
     }
+
 }
