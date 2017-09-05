d622e934464702c38b5c3a12115a1448fbdb49b5
bangnl <luongbangvh@gmail.com>
2017-09-05 07:07:15 +0700

    write login component

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { LoginForm } from "../../model/login-form";
 import { AuthenticationService } from "../../services/authenticaiton.service"
+import {User} from "../../model/user";
+import {AppSettings} from '../../app-settings'
 
 @Component({
   selector: 'login-component',
@@ -11,8 +13,15 @@ export class LoginComponent{
   constructor(private authenticationService: AuthenticationService ){}
 
   model = new LoginForm();
+  loginError = false;
 
   onSubmit() {
-    this.authenticationService.login(this.model);
+    this.authenticationService.login(this.model).subscribe((user: User) => {
+      console.log(localStorage.getItem(AppSettings.USER_INFORMATION))
+    },
+    error => {
+      this.loginError = true;
+    }
+    );
   }
 }
--- client/src/app/services/authenticaiton.service.ts
+++ client/src/app/services/authenticaiton.service.ts
@@ -5,14 +5,20 @@ import {Api} from "../api";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 import {Observable} from "rxjs/Observable";
+import {User} from "../model/user";
+import {AppSettings} from "../app-settings"
 
 @Injectable()
 export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
-  login(loginForm: LoginForm) {
-    this.http.post(Api.LOGIN_API,loginForm).map((res: Response) => {
-      console.log(res.json())
-    }).catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  login(loginForm: LoginForm): Observable<User>{
+    return this.http.post(Api.LOGIN_API,loginForm).map((res: Response) => {
+      let user = new User();
+      user.username = res["username"];
+      user.role = res["authorities"][0]["authority"];
+      localStorage.setItem(AppSettings.USER_INFORMATION, JSON.stringify(user));
+      return user;
+    }).catch(error => Observable.throw(error));
   }
 }
